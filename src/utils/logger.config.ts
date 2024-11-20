2c0af2109112f0fcf1d83cfd258022346ce0e073
ezehuche <ezehuche4@gmail.com>
2024-11-20 01:36:12 +0100

    fixed path module issue

--- src/utils/logger.config.ts
+++ src/utils/logger.config.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import { createLogger, format, transports } from 'winston';
 import 'winston-daily-rotate-file';
 
